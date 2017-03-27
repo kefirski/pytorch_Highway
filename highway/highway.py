70b75db7a2d029f4bbe08fd4c7d69e36bf7b6d3a
Daniil Gavrilov <olavursky@gmail.com>
2017-03-27 20:02:57 +0300

    redimental method removed

--- highway/highway.py
+++ highway/highway.py
@@ -4,19 +4,19 @@ import torch.nn.functional as F
 
 class Highway(nn.Module):
     def __init__(self, size, num_layers, f):
-        
+
         super(Highway, self).__init__()
-        
+
         self.num_layers = num_layers
-        
+
         self.nonlinear = nn.ModuleList([nn.Linear(size, size) for _ in range(num_layers)])
-        
+
         self.linear = nn.ModuleList([nn.Linear(size, size) for _ in range(num_layers)])
-        
+
         self.gate = nn.ModuleList([nn.Linear(size, size) for _ in range(num_layers)])
-        
+
         self.f = f
-    
+
     def forward(self, x):
         """
             :param x: tensor with shape of [batch_size, size]
@@ -25,17 +25,13 @@ class Highway(nn.Module):
             f is non-linear transformation, σ(x) is affine transformation with sigmoid non-linearition
             and ⨀ is element-wise multiplication
             """
-        
+
         for layer in range(self.num_layers):
             gate = F.sigmoid(self.gate[layer](x))
-            
+
             nonlinear = self.f(self.nonlinear[layer](x))
             linear = self.linear[layer](x)
-            
+
             x = gate * nonlinear + (1 - gate) * linear
-        
+
         return x
-    
-    def _add_to_parameters(self, parameters, name):
-        for i, parameter in enumerate(parameters):
-            self.register_parameter(name='{}-{}'.format(name, i), param=parameter)
